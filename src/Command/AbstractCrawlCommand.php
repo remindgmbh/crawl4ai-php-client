cf3762c3837984816cd6dbf0efe80cbc1a7ade52
Dominik Lehrl <d.lehrl@remind.de>
2026-06-02 21:29:32 +0200

    feat: add output file writer for crawl results
    - Add command arguments and options
    - Provide optional gzip compression for output files
    - Set default crawler configs

--- src/Command/AbstractCrawlCommand.php
+++ src/Command/AbstractCrawlCommand.php
@@ -6,10 +6,34 @@ namespace App\Command;
 
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\DependencyInjection\Attribute\Autowire;
+use Symfony\Component\Filesystem\Exception\IOExceptionInterface;
+use Symfony\Component\Filesystem\Filesystem;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 abstract class AbstractCrawlCommand extends Command
 {
+    protected const DEFAULT_CRAWLER_CONFIG = [
+        'cache_mode' => 'BYPASS',
+        'word_count_threshold' => 10,
+        'excluded_tags' => ['nav', 'footer', 'header', 'aside'],
+        'remove_overlay_elements' => true,
+        'remove_consent_popups' => true,
+        'markdown_generator' => [
+            'type' => 'DefaultMarkdownGenerator',
+            'params' => [
+                'content_source' => 'cleaned_html',
+                'content_filter' => [
+                    'type' => 'PruningContentFilter',
+                    'params' => [
+                        'threshold' => 0.5,
+                        'threshold_type' => 'fixed',
+                        'min_word_threshold' => 10,
+                    ],
+                ],
+            ],
+        ],
+    ];
+
     public function __construct(
         #[Autowire(env: 'CRAWL4AI_BASE_URL')]
         protected string $baseUrl,
@@ -18,12 +42,42 @@ abstract class AbstractCrawlCommand extends Command
         parent::__construct();
     }
 
-    protected function crawl(string $url, string $endpoint = '/crawl'): array
+    protected function crawl(array $urls, string $endpoint = '/crawl', string $locale = 'en-EN'): array
     {
         $response = $this->client->request('POST', $this->baseUrl . $endpoint, [
-            'json' => ['urls' => [$url]],
+            'json' => [
+                'urls' => $urls,
+                'crawler_config' => array_merge(self::DEFAULT_CRAWLER_CONFIG, ['locale' => $locale]),
+            ],
         ]);
 
         return $response->toArray();
+    }
+
+    protected function writeOutputFile(string $content, string $sitemapUrl, string $outputFileNamePrefix, bool $fileCompression): void
+    {
+        $fileName = $outputFileNamePrefix . '-'
+            . parse_url($sitemapUrl, PHP_URL_HOST)
+            . '-' . date('Y-m-d-H-i-s')
+            . '.json';
+
+        $filesystem = new Filesystem();
+        $outputDir = __DIR__ . '/../../crawl/output';
+
+        try {
+            $filesystem->mkdir($outputDir);
+            $filePath = $outputDir . '/' . $fileName;
+            $filesystem->dumpFile($filePath, $content);
+
+            if ($fileCompression) {
+                $gzipPath = $filePath . '.gz';
+                $gzFile = gzopen($gzipPath, 'wb6');
+                gzwrite($gzFile, $content);
+                gzclose($gzFile);
+                $filesystem->remove($filePath);
+            }
+        } catch (IOExceptionInterface $exception) {
+            echo "An error occurred while creating your directory at " . $exception->getPath();
+        }
     }
 }
--- src/Command/CrawlSitemapXmlCommand.php
+++ src/Command/CrawlSitemapXmlCommand.php
@@ -4,28 +4,53 @@ declare(strict_types=1);
 
 namespace App\Command;
 
-use Symfony\Component\Console\Attribute\Argument;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 #[AsCommand(name: 'crawl4ai:sitemap')]
 class CrawlSitemapXmlCommand extends AbstractCrawlCommand
 {
     public function __invoke(
-        #[Argument('The URL of the sitemap.')] string $sitemapUrl,
-        OutputInterface $output
+        InputInterface $input,
+        OutputInterface $output,
     ): int {
+        $startTime = time();
+        $sitemapUrl = $input->getArgument('sitemapUrl');
+        $outputFileNamePrefix = $input->getOption('outputFileNamePrefix');
+        $locale = $input->getOption('locale');
+        $fileCompression = $input->getOption('fileCompression');
+
         $output->writeln('Reading sitemap: ' . $sitemapUrl);
         $urls = $this->extractUrlsFromSitemap($sitemapUrl);
         $output->writeln('URLs found: ' . count($urls));
 
-        foreach ($urls as $url) {
-            $output->writeln('Crawling URL: ' . $url);
-            // $this->crawl($url);
-        }
+
+        $markdown = json_encode(
+            $this->crawl(
+                urls: $urls,
+                locale: $locale
+            ),
+            JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE
+        );
+
+        $this->writeOutputFile($markdown, $sitemapUrl, $outputFileNamePrefix, $fileCompression);
+
+        $output->writeln('Process duration: ' . (time() - $startTime) . 's');
         
         return Command::SUCCESS;
+    }
+
+    protected function configure(): void
+    {
+        $this
+            ->addArgument('sitemapUrl', InputArgument::REQUIRED)
+            ->addOption('outputFileNamePrefix', null, InputOption::VALUE_OPTIONAL, 'Prefix for the output file name', 'crawl')
+            ->addOption('locale', null, InputOption::VALUE_OPTIONAL, 'Locale for the crawl', 'en-EN')
+            ->addOption('fileCompression', null, InputOption::VALUE_NONE, 'Compress output file with gzip');
     }
 
     protected function extractUrlsFromSitemap(string $sitemapUrl): array
